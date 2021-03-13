0d62fc62ce9fddd1474a982a553cbd8e98bba0d6
Rob <robbiejohnwallace@yahoo.co.uk>
2021-03-13 23:46:31 +0000

    Added working functions for npcs to get drinks. (#10)

---
+++ Stacshack-2021-unity/Assets/Scripts/CupTrigger.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupTrigger : MonoBehaviour{
+    private void OnTriggerStay(Collider other) {
+        other.gameObject.GetComponent<DrinkSubmit>().SubmitDrink(this.gameObject);
+    }
+}
