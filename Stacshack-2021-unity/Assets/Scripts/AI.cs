a9dc5e363b59e855068947498792db8479b9a23a
Rob <robbiejohnwallace@yahoo.co.uk>
2021-03-13 22:15:19 +0000

    Added basic NPC AI

--- Stacshack-2021-unity/Assets/Scripts/AI.cs
+++ Stacshack-2021-unity/Assets/Scripts/AI.cs
@@ -1,36 +1,133 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class AI : MonoBehaviour {
+ public class AI : MonoBehaviour {
 
-//     // Thirst level of the AI
-//     [Range(0, 1)]
-//     public float thirst;
-    
-//     // Drunkness level of the AI
-//     [Range(0, 1)]
-//     public float drunkness;
+    // Thirst level of the AI
+    [Range(0, 1)]
+    public float thirst;
+    public float thirst_threshold = 0.25f;
+    public float thirst_reduce_rate = 0.01f;
 
-//     // Start is called before the first frame update
-//     void Start() {
-        
-//     }
+    public Transform idle_location;
+    public Transform order_location;
 
-//     // Update is called once per frame
-//     void Update() {
+    public MoveTo moveTo;
+    public GameObject barManger;
 
-//         // Increment thirst level 
-//         thirst += 0.01;
 
-//         // If Thirsty enough, go get a drink
-//         if (thirst > 0.5) {
-//             // Get drink
-//         }
-//     }
+    // Current State of the AI
+    AIStateType current_state;
 
-//     void GetDrink() {
-//         return 0;
-//     }
 
-// }
+    // Start is called before the first frame update
+    void Start() {
+        // Set state to moving 
+        this.current_state = AIStateType.moving;
+
+        // Move to idle location
+        this.Move(idle_location, AIStateType.idle);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Reduce The Ai's thirs 
+        this.thirst -= this.thirst_reduce_rate * Time.deltaTime;
+
+        // Handle the state 
+        this.HandleState();
+    }
+
+    void UpdateState(AIStateType new_type) {
+        // Decide how to handle this state transition 
+        switch (new_type) {
+            case AIStateType.idle : {
+                // Set state to idle 
+                this.current_state = new_type;
+                break;
+            }
+            case AIStateType.order: {
+                // Order a drink 
+                this.current_state = new_type;
+                break;
+            }
+            case AIStateType.waiting_for_order: {
+                // Ordering a drink 
+                this.current_state = new_type;
+                break;
+            }
+            default: {
+                print("Unkown State Type");
+                break;
+            }
+        }
+    }
+
+
+    void HandleState() {
+        // Decide how to handle this state transition 
+        switch (this.current_state) {
+            case AIStateType.idle: {
+                // Idle 
+                Idle();
+                break;
+            }
+            case AIStateType.order: {
+                // Order a drink 
+                Order();
+                break;
+            }
+            case AIStateType.waiting_for_order: {
+                // Do Nothing
+                break;
+            }
+            case AIStateType.moving: {
+                 // Do nothing 
+                 break;
+            }
+            default: {
+                print("Unkown State Type");
+                break;
+            }
+        }
+    }
+
+    void Idle() {
+        // Check if too firsty 
+        if(this.thirst <= this.thirst_threshold) {
+            // Check if there space 
+            if (this.barManger.GetComponent<Bar>().RequestSpace()) {
+                // Move to the bar 
+                this.Move(this.order_location, AIStateType.order);
+            }
+        }
+    }
+
+
+    void Order() {
+        // Order a drink 
+        this.barManger.GetComponent<Bar>().OrderDrink(new System.Action<AIStateType>(this.UpdateState), AIStateType.order_complete);
+
+        // Update to waiting for order 
+        this.UpdateState(AIStateType.waiting_for_order);
+    }
+
+
+    void Move(Transform goal, AIStateType callback_state) {
+        // Set state to moving 
+        this.current_state = AIStateType.moving;
+
+        // Move to location 
+        this.moveTo.MoveToLocation(goal, new System.Action<AIStateType>(this.UpdateState), callback_state);
+    }
+ }
+
+
+public enum AIStateType {
+    idle, 
+    moving,
+    order,
+    waiting_for_order,
+    order_complete
+}
--- Stacshack-2021-unity/Assets/Scripts/MoveTo.cs
+++ Stacshack-2021-unity/Assets/Scripts/MoveTo.cs
@@ -3,45 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MoveTo : MonoBehaviour
-{
-    public Transform goal;
-    private bool isMoving;
-    // Start is called before the first frame update
-    void Start()
-    {
+public class MoveTo : MonoBehaviour{
+    // Keep track of if we are moving 
+    bool moving = false;
+
+    // Keep track of call back function to say we have reached goal 
+    System.Action<AIStateType> callback;
+    AIStateType callback_arg;
+
+    public void MoveToLocation(Transform goal, System.Action<AIStateType> callback, AIStateType callback_arg) {
         // Getting the agent
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
 
-        // Setting the path 
+        // Set the path 
         agent.destination = goal.position;
 
-        // Setting isMoving
-        isMoving = true;
-        
-        
+        // Set moving to true 
+        this.moving = true;
+
+        // Store call back 
+        this.callback = callback;
+        this.callback_arg = callback_arg;
     }
 
-    // void generateNewGoal() {
 
-    // }
+    private void Update() {
+        if (moving) {
+            // Check if we have reached the goal location 
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
 
-    // Update is called once per frame
-    void Update() 
-    {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            if (!agent.pathPending) {
+                if (agent.remainingDistance <= agent.stoppingDistance) {
+                    if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f) {
+                        // Reached our goal location 
+                        this.moving = false;
 
-        // Checking if the agent is still moving
-        if(isMoving) {
-            // Checking if the agent is still on a path
-            isMoving = agent.hasPath;
-
-            return;
-        } else { // Not moving
-            // Try and generate new path
+                        // Call back to A.I to let know the goal has been achinved 
+                        this.callback(this.callback_arg);
+                    }
+                }
+            }
         }
-
-        print(agent.hasPath);
     }
-
 }
+
+
