330f2d43abd210456d05830d30ab865893b070bf
Rob <robbiejohnwallace@yahoo.co.uk>
2021-03-13 09:50:16 +0000

    Added person prefab.

---
+++ Stacshack-2021-unity/Assets/Scripts/SkinUpdatorEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(SkinUpdator))]
+public class SkinUpdatorEditor : Editor {
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector(); // Draw deafult inspector vairables 
+
+		SkinUpdator skinUpdator = (SkinUpdator)target; // Get access to the skin updator 
+
+		if (GUILayout.Button("Refresh")) {
+			skinUpdator.Refresh();
+		}
+	}
+}
