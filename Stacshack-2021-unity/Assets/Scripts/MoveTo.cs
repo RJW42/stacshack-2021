a600bd9ea992700abae46767507e97694e9b9600
FindlaySloan <47221996+FindlaySloan@users.noreply.github.com>
2021-03-13 21:10:47 +0000

    Initail Commit

--- Stacshack-2021-unity/Assets/Scripts/MoveTo.cs
+++ Stacshack-2021-unity/Assets/Scripts/MoveTo.cs
@@ -5,19 +5,43 @@ using UnityEngine.AI;
 
 public class MoveTo : MonoBehaviour
 {
-
     public Transform goal;
-
+    private bool isMoving;
     // Start is called before the first frame update
     void Start()
     {
+        // Getting the agent
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Setting the path 
+        agent.destination = goal.position;
+
+        // Setting isMoving
+        isMoving = true;
+        
         
     }
+
+    // void generateNewGoal() {
+
+    // }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+
+        // Checking if the agent is still moving
+        if(isMoving) {
+            // Checking if the agent is still on a path
+            isMoving = agent.hasPath;
+
+            return;
+        } else { // Not moving
+            // Try and generate new path
+        }
+
+        print(agent.hasPath);
+    }
+
 }
