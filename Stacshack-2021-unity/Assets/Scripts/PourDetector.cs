18ea8fed3fee2b0401728ab5c200fc13a9b46ac5
Rob <robbiejohnwallace@yahoo.co.uk>
2021-03-13 14:12:23 +0000

    Added bottle

---
+++ Stacshack-2021-unity/Assets/Scripts/PourDetector.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class PourDetector : MonoBehaviour {
+    public int pourThreshold = 45;
+    public Transform origin = null;
+    public GameObject streamPrefab = null;
+    public Color liquid_color;
+
+    private bool isPouring = false;
+    private Stream currentStream = null;
+
+    void Update() {
+        // Check if we are pouring 
+        bool pour_check = this.CalculatePourAngle() < this.pourThreshold;
+
+        // Start or stop pouring 
+        if(this.isPouring != pour_check) {
+            this.isPouring = pour_check;
+
+            if (this.isPouring) {
+                this.StartPour();
+            }
+            else {
+                this.EndPour();
+            }
+        }
+    }
+
+    
+    void StartPour() {
+        currentStream = CreateStream();
+        currentStream.Begin();
+        currentStream.SetColor(this.liquid_color);
+    }
+
+
+    void EndPour() {
+        currentStream.End();
+        currentStream = null;
+    }
+
+
+    float CalculatePourAngle() {
+        return transform.forward.y * Mathf.Rad2Deg;
+    }
+
+
+    Stream CreateStream() {
+        // Create stream gameobject 
+        GameObject streamObject = Instantiate(streamPrefab, origin.position, Quaternion.identity, transform);
+
+        return streamObject.GetComponent<Stream>();
+    }
+}
---
+++ Stacshack-2021-unity/Assets/Scripts/Stream.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stream : MonoBehaviour {
+    public float flow_distance = 2.0f;
+    public float stream_speed = 1.75f;
+
+    private LineRenderer lineRenderer = null;
+    private ParticleSystem splashParticle = null;
+
+    private Coroutine pourRoutine = null;
+    private Vector3 targetPosition = Vector3.zero;
+
+    void Awake() {
+        // Called on instantion 
+        this.lineRenderer = GetComponent<LineRenderer>();
+        this.splashParticle = GetComponentInChildren<ParticleSystem>();
+    }
+
+    void Start() {
+        // Make sure line render in correct place 
+        MoveToPosition(0, transform.position);
+        MoveToPosition(1, transform.position);
+    }
+
+    public void SetColor(Color color) {
+        // Update the stream color 
+        Material material = GetComponent<Renderer>().material;
+        material.SetColor("_Color", color);
+
+        // Update the particles color 
+        var main = splashParticle.main;
+        main.startColor = color;
+    }
+
+    public void Begin() {
+        StartCoroutine(this.UpdateParticle());
+
+        this.pourRoutine = StartCoroutine(this.BeginPour());
+    }
+
+    IEnumerator BeginPour() {
+        while (gameObject.activeSelf) {
+            // While active 
+            targetPosition = FindEndPoint();
+
+            MoveToPosition(0, transform.position);
+            AnimateToPosition(1, targetPosition);
+
+            yield return null;
+        }
+    }
+
+    public void End() {
+        StopCoroutine(this.pourRoutine);
+        this.pourRoutine = StartCoroutine(EndPour());
+    }
+
+    IEnumerator EndPour() {
+        while (!HasReachedPosition(0, this.targetPosition)) {
+            // Move both ends of stream to target poition 
+            AnimateToPosition(0, this.targetPosition);
+            AnimateToPosition(1, this.targetPosition);
+
+            yield return null;
+        }
+
+        // Once starting point reached ground destroy stream 
+        Destroy(gameObject);
+    }
+
+    Vector3 FindEndPoint() {
+        // Init some variables 
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        // Find end position of stream 
+        Physics.Raycast(ray, out hit, flow_distance);
+        Vector3 endPoint = hit.collider ? hit.point : ray.GetPoint(flow_distance);
+
+        return endPoint;
+    }
+
+    void MoveToPosition(int index, Vector3 target_position) {
+        lineRenderer.SetPosition(index, target_position);
+    }
+
+    void AnimateToPosition(int index, Vector3 target_position) {
+        Vector3 current_point = this.lineRenderer.GetPosition(index);
+        Vector3 new_position = Vector3.MoveTowards(current_point, target_position, Time.deltaTime * this.stream_speed);
+
+        lineRenderer.SetPosition(index, new_position);
+    }
+
+    bool HasReachedPosition(int index, Vector3 target_position) {
+        Vector3 current_position = lineRenderer.GetPosition(index);
+
+        return current_position == target_position;
+    }
+
+    IEnumerator UpdateParticle() {
+        while (gameObject.activeSelf) {
+            splashParticle.gameObject.transform.position = this.targetPosition;
+
+            bool is_hitting = HasReachedPosition(1, this.targetPosition);
+            splashParticle.gameObject.SetActive(is_hitting);
+
+            yield return null;
+        }
+    }
+}
